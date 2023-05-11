6acce746a6b61e1a7c4f95de7b279dcf5f9179dd
joaovicttor19 <111836155+joaovicttor19@users.noreply.github.com>
2023-05-10 21:23:48 -0300

    atualizado

--- src/main/kotlin/com/company/confinance/model/MovementModel.kt
+++ src/main/kotlin/com/company/confinance/model/MovementModel.kt
@@ -1,22 +1,35 @@
 package com.company.confinance.model
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.*
 import javax.persistence.*
 
 @Entity
-@Table( name = "Movement")
+@Table(name = "Movement")
 
 data class MovementModel(
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-var id: Long? = null,
-var type_movement: String,
-var value: Double,
-var description: String,
-var date: Date,
-@ManyToOne
-var user: UserModel
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    var type_movement: String,
+
+    @Column(nullable = false)
+    var value: Double,
+
+    var description: String,
+    var date: LocalDate,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonProperty("user_id")
+    val user: UserModel
+
 )
-
-
 
