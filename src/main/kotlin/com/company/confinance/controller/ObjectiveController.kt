f56381114af9be7e1252b8cd9856b4e3f9fcaaeb
Bruna Camilly <camillybrubs@gmail.com>
2023-06-01 23:55:11 -0300

    Resposta json

--- src/main/kotlin/com/company/confinance/controller/ObjectiveController.kt
+++ src/main/kotlin/com/company/confinance/controller/ObjectiveController.kt
@@ -24,61 +24,128 @@ class ObjectiveController {
 
     @Autowired
     private lateinit var repository: ObjectiveRepository
+
     @Autowired
     private lateinit var userRepository: UserRepository
 
     @PostMapping
-    fun createObjective(
-        @RequestBody objective : ObjectiveModel
-    ): ObjectiveModel {
-        return  repository.save(objective)
+    fun createObjective(@RequestBody objective: ObjectiveModel): ResponseEntity<Any> {
+        return try {
+            ResponseEntity.status(HttpStatus.CREATED).body(repository.save(objective))
+        } catch (ex: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar o objetivo")
+        }
     }
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long) {
-        return repository.deleteById(id)
-    }
+    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<Any> {
+        val existingObjective = repository.findById(id)
+        return when {
+            id <= 0 -> ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                CustomResponse("ID do objetivo inválido. Informe um ID válido", HttpStatus.BAD_REQUEST.value())
+            )
 
+            existingObjective.isPresent -> {
+                repository.deleteById(id)
+                ResponseEntity.ok().body(
+                    CustomResponse(
+                        "Objetivo deletado com sucesso!",
+                        HttpStatus.OK.value()
+                    )
+                )
+            }
+
+            else -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                CustomResponse(
+                    "Não foi possível deletar o objetivo, verifique o ID informado.",
+                    HttpStatus.NOT_FOUND.value()
+                )
+            )
+        }
+    }
 
     @PutMapping("/{id}")
     fun updateObjectiveById(
-        @PathVariable (value = "id") id: Long,
+        @PathVariable(value = "id") id: Long,
         @RequestBody objective: ObjectiveModel
-    ): ResponseEntity<ObjectiveModel> {
-        val existingObjective = repository.findById(id)
-        if (existingObjective.isPresent) {
-            val updateObjective = existingObjective.get()
-            updateObjective.value = objective.value
-            updateObjective.name = objective.name
-            updateObjective.date = objective.date
-            repository.save(updateObjective)
-            return ResponseEntity.ok(updateObjective)
+    ): ResponseEntity<Any> {
+        return if (repository.existsById(id)) {
+            val existingObjective = repository.findById(id).get()
+            existingObjective.value = objective.value
+            existingObjective.description = objective.description
+            existingObjective.date = objective.date
+            repository.save(existingObjective)
+            ResponseEntity.ok(existingObjective)
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                CustomResponse(
+                    "Não foi possível atualizar o objetivo, por favor verifique o ID e tente novamnte.",
+                    HttpStatus.NOT_FOUND.value()
+                )
+            )
         }
-        return ResponseEntity.notFound().build()
     }
 
-
-
     @GetMapping("/{id}")
-    fun getObjectiveId(
-        @PathVariable("id") id: Long,
-    ): ObjectiveModel {
-        return repository.findById(id).orElseThrow(){
-            EntityNotFoundException()
+    fun getObjectiveById(@PathVariable("id") id: Long): ResponseEntity<Any> {
+        return if (id <= 0) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                CustomResponse(
+                    "Erro, o ID informado é inválido. Por favor, insira um ID correto.",
+                    HttpStatus.BAD_REQUEST.value()
+                )
+            )
+        } else {
+            val objective = repository.findById(id)
+            if (objective.isPresent) {
+                ResponseEntity.ok(objective.get())
+            } else {
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    CustomResponse(
+                        "Objetivo não encontrado, verifique o ID e tente novamente.",
+                        HttpStatus.NOT_FOUND.value()
+                    )
+                )
+            }
         }
     }
 
     @GetMapping
-    fun getObjective(): List<ObjectiveModel> {
-        return repository.findAll()
+    fun getObjectives(): ResponseEntity<Any> {
+        return if (repository.findAll().isNotEmpty()) {
+            ResponseEntity.ok(repository.findAll())
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                CustomResponse(
+                    "Nenhum objetivo encontrado",
+                    HttpStatus.NOT_FOUND.value()
+                )
+            )
+        }
     }
 
     @GetMapping("/user/{userId}")
-    fun getObjectiveByUserId(
-        @PathVariable("userId") userId: Long
-    ): List<ObjectiveModel> {
-        return repository.findByUserId(userId)
+    fun getObjectivesByUserId(@PathVariable("userId") userId: Long): ResponseEntity<Any> {
+        return if (userId <= 0) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                CustomResponse(
+                    "Erro, ID inválido. Por favor, informe um ID correto.",
+                    HttpStatus.BAD_REQUEST.value()
+                )
+            )
+        } else {
+            val user = userRepository.findById(userId)
+            if (user.isPresent) {
+                val objectives = repository.findByUserId(userId)
+                ResponseEntity.ok(objectives)
+            } else {
+                ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                    CustomResponse(
+                        "Nenhum objetivo encontrado para o usuário",
+                        HttpStatus.NOT_FOUND.value()
+                    )
+                )
+            }
+        }
     }
-
-
 }
